dec7dcbde94b7bfebf900fd0d615466618196164
Abdulkadir <abdulkadiryaman@gmail.com>
2017-09-29 10:58:35 +0300

    passing extra dns paremeter

--- start-openconnect.sh
+++ start-openconnect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -n "$OPENCONNECT_HOST" ]; then
   echo "starting openconnect juniper vpn...."
-  echo "${OPENCONNECT_PASSWORD}" |  openconnect --juniper ${OPENCONNECT_HOST} --script=/etc/vpnc-script -u ${OPENCONNECT_USER} --passwd-on-stdin --servercert ${OPENCONNECT_SERVER_CERT}
+  echo "${OPENCONNECT_PASSWORD}" |  openconnect --juniper ${OPENCONNECT_HOST} -u ${OPENCONNECT_USER} --passwd-on-stdin --servercert ${OPENCONNECT_SERVER_CERT}
   
   if [ -n "$DNS" ]; then
     cat "nameserver ${DNS}" >> /etc/resolv.conf
